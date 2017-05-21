aa02afe1ba0da00c1bef060ad4375065e70fefd9
n81ur3 <helmut.bierbaumer@gmail.com>
2017-05-21 09:39:06 +0200

    First working version

--- uebung01_bierbaumer/src/main/java/at/fhj/swd/HtmlQuestionWriter.java
+++ uebung01_bierbaumer/src/main/java/at/fhj/swd/HtmlQuestionWriter.java
@@ -1,5 +1,7 @@
 package at.fhj.swd;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 /**
@@ -19,8 +21,23 @@ public class HtmlQuestionWriter {
     private final static String FOOTER = "</body>\n</html>";
 
     public void writeQuestion(String location, ArrayList<Question> questions) {
-       for (Question q : questions) {
-           System.out.println(q.getQuestion());
-       }
+        try {
+            PrintWriter writer = new PrintWriter(location);
+            writer.write(HEADER);
+            writer.write("<h2>Fragen:</h2>");
+            for (Question q : questions) {
+                writer.write("<h3>" + q.getQuestion() + "</h3>");
+                writer.write("<ol>");
+                for (String answer : q.getAnswers()) {
+                    writer.write("<li>" + answer + "</li>");
+                }
+                writer.write("</ol>");
+            }
+            writer.write(FOOTER);
+            writer.flush();
+            writer.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 }
--- uebung01_bierbaumer/src/main/java/at/fhj/swd/QuestionReader.java
+++ uebung01_bierbaumer/src/main/java/at/fhj/swd/QuestionReader.java
@@ -1,7 +1,6 @@
 package at.fhj.swd;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 
@@ -12,21 +11,73 @@ import java.util.ArrayList;
 public class QuestionReader {
 
     public ArrayList<Question> readQuestions(String location) {
+        ArrayList<Question> questions = new ArrayList<>();
 
         try {
             //BufferedReader in = new BufferedReader(new FileReader("/mnt/data/Personal_data/OneDrive/Documents/Privat/FH/SS2017/SoftDevel2/soft_devel_2_projects/01_exercise_bierbaumer/src/main/resources/sample_quiz.csv"));
             BufferedReader in = new BufferedReader(new FileReader(location));
             //BufferedReader in = new BufferedReader(new FileReader(new File(getClass().getClassLoader().getResource("sample_quiz.csv").getPath())));
-            String s;
-            StringBuilder sb = new StringBuilder();
-            while ((s = in.readLine()) != null) {
-                sb.append(s);
-                sb.append("\n");
+            String line;
+            while ((line = in.readLine()) != null) {
+                if (!(line.startsWith("#")) && !(line.isEmpty()) && isValidLine(line)) {
+                    questions.add(getQuestionFromLine(line));
+                }
             }
-            System.out.println(sb.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+
+        return questions;
+    }
+
+    private boolean isValidLine(String line) {
+       String answerText;
+       String correctAnswer;
+
+       String[] parts = line.split(";");
+       if (parts.length != 3) {
+           return false;
+       }
+
+       for (String s : parts) {
+           if (s.isEmpty()) {
+               return false;
+           }
+       }
+
+       answerText = parts[1];
+       correctAnswer = parts[2];
+
+       if(!(checkAnswerIndex(answerText, correctAnswer))) {
+          return false;
+       }
+
+        return true;
+    }
+
+    private boolean checkAnswerIndex(String answerText, String correctAnswer) {
+
+        if (!(correctAnswer.matches("[0-4]"))) {
+            return false;
+        }
+
+        String[] answers = answerText.split("/");
+        int correctIndex = Integer.parseInt(correctAnswer);
+
+        if (answers.length < 2 || correctIndex > answers.length) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private Question getQuestionFromLine(String line) {
+        String[] parts = line.split(";");
+        String question = parts[0];
+        String answerText = parts[1];
+        String[] answers = answerText.split("/");
+        int correctAnswer = Integer.parseInt(parts[2]);
+
+        return new Question(question, answers, correctAnswer);
     }
 }
